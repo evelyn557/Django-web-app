961039834794a0f41f89722ae10959383c1096e3
Evelyn Sepulveda <evelynsepulvedavarela@gmail.com>
2026-09-13 01:05:48 -0300

    Primer commit

---
+++ tareas/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import Proyecto, Tarea
+
+class RegistroUsuarioForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+class ProyectoForm(forms.ModelForm):
+    class Meta:
+        model = Proyecto
+        fields = ["nombre", "descripcion"]
+        
+        # El método va aquí adentro, alineado con 'class Meta'
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        
+        if len(nombre) < 4:
+            raise forms.ValidationError("El nombre del proyecto debe tener al menos 4 caracteres.")
+            
+        if "prueba" in nombre.lower():
+            raise forms.ValidationError("El nombre no puede contener la palabra 'prueba'.")
+            
+        return nombre
+
+class TareaForm(forms.ModelForm):
+    class Meta:
+        model = Tarea
+        fields = ["titulo", "descripcion", "estado"]
+        
+       
